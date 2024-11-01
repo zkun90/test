e57e450d6c56e9dd6ceb2a479bd5026e41b61e4d
zkun <zkun@qdu.edu.cn>
2024-11-02 00:19:37 +0800

    add test.sh

---
+++ test.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+
+# This script is used to generate the list of all the files in the directory
+# and its subdirectories. The list is then used to generate the checksums of
+# all the files in the directory and its subdirectories. The checksums are
+# stored in a file named checksums.txt in the directory.
+
+sleep 1
